8c8070788e33d7d0ee6e96c09029bb8ffe752743
asrient <asen182000@hotmail.com>
2021-10-04 13:45:46 +0530

    air peer test

--- lib/airPeer.dart
+++ lib/airPeer.dart
@@ -1,11 +1,17 @@
 //import 'package:multicast_dns/multicast_dns.dart';
 import 'dart:io';
-import 'package:dns/dns.dart';
+import 'dns_packet.dart';
 import 'dart:async';
 //import 'package:device_info_plus/device_info_plus.dart';
 import 'package:eventify/eventify.dart';
-import 'package:raw/raw.dart';
+import 'raw/raw.dart';
 import 'dart:typed_data';
+import 'package:cryptography/cryptography.dart';
+import 'dart:convert';
+import "package:pointycastle/export.dart" as c1;
+import 'dart:math';
+import 'package:flutter/foundation.dart';
+import "package:asn1lib/asn1lib.dart";
 //import 'package:buffer/buffer.dart';
 
 ///////////////////////////////////////
@@ -321,7 +327,7 @@ class Message {
   AirId? to;
   AirId? from;
   int? status;
-  var body;
+  late ByteData body;
 
   Message(
       {required this.type,
@@ -472,24 +478,12 @@ class Message {
         msg += "status=" + status!.toString() + sep;
       }
     }
-
     msg += sep;
-    if (body is String || body == null) {
-      msg += body;
-      return Uint8List.fromList(msg.codeUnits).buffer.asByteData();
-    }
     var headerList = msg.codeUnits;
     var bodyList = body.buffer.asUint8List();
     bodyList.insertAll(0, headerList);
     ByteData buff = bodyList.buffer.asByteData();
     return buff;
-  }
-
-  ByteData bodyAsBuffer() {
-    if (body is String) {
-      return Uint8List.fromList(body.codeUnits).buffer.asByteData();
-    }
-    return body as ByteData;
   }
 }
 
@@ -503,7 +497,7 @@ class MessageStream {
       m = Message.tryDecode(buff);
       if (m != null) {
         onReady(this);
-        _s.add(m!.bodyAsBuffer());
+        _s.add(m!.body);
       }
     } else
       _s.add(buff);
@@ -666,12 +660,7 @@ class Local {
   }
 }
 
-main() {
-  var x = AirId(airId: "gfh");
-  getIpAddr().then((ip) {
-    print(ip);
-  });
-  print(getDeviceName());
+bonjourTest() {
   Bonjour b = Bonjour();
   b.on("query", null, (env, _) {
     DnsData d = env.eventData as DnsData;
@@ -681,4 +670,288 @@ main() {
     DnsData d = env.eventData as DnsData;
     d.prnt();
   });
+}
+
+class RSA_ParametersWithRandom<UnderlyingParameters extends c1.CipherParameters>
+    implements c1.CipherParameters {
+  final UnderlyingParameters parameters;
+  final c1.SecureRandom random;
+
+  RSA_ParametersWithRandom(this.parameters, this.random);
+}
+
+class RSA {
+  static c1.AsymmetricKeyPair<c1.PublicKey, c1.PrivateKey> getRsaKeyPair(
+      c1.SecureRandom secureRandom) {
+    var rsapars = new c1.RSAKeyGeneratorParameters(BigInt.from(65537), 2048, 5);
+    var params = new RSA_ParametersWithRandom(rsapars, secureRandom);
+    var keyGenerator = new c1.RSAKeyGenerator();
+    keyGenerator.init(params);
+    return keyGenerator.generateKeyPair();
+  }
+
+  static c1.SecureRandom getSecureRandom() {
+    var secureRandom = c1.FortunaRandom();
+    var random = Random.secure();
+    List<int> seeds = [];
+    for (int i = 0; i < 32; i++) {
+      seeds.add(random.nextInt(255));
+    }
+    secureRandom.seed(new c1.KeyParameter(new Uint8List.fromList(seeds)));
+    return secureRandom;
+  }
+
+  static Future<c1.AsymmetricKeyPair<c1.PublicKey, c1.PrivateKey>>
+      computeRSAKeyPair() async {
+    return await compute(getRsaKeyPair, getSecureRandom());
+  }
+
+  /// Decode Public key from PEM Format
+  ///
+  /// Given a base64 encoded PEM [String] with correct headers and footers, return a
+  /// [RSAPublicKey]
+  ///
+  /// *PKCS1*
+  /// RSAPublicKey ::= SEQUENCE {
+  ///    modulus           INTEGER,  -- n
+  ///    publicExponent    INTEGER   -- e
+  /// }
+  ///
+  /// *PKCS8*
+  /// PublicKeyInfo ::= SEQUENCE {
+  ///   algorithm       AlgorithmIdentifier,
+  ///   PublicKey       BIT STRING
+  /// }
+  ///
+  /// AlgorithmIdentifier ::= SEQUENCE {
+  ///   algorithm       OBJECT IDENTIFIER,
+  ///   parameters      ANY DEFINED BY algorithm OPTIONAL
+  /// }
+  static c1.RSAPublicKey parsePublicKeyFromPem(pemString) {
+    List<int> publicKeyDER = decodePEM(pemString);
+    var asn1Parser = new ASN1Parser(Uint8List.fromList(publicKeyDER));
+    var topLevelSeq = asn1Parser.nextObject() as ASN1Sequence;
+
+    var modulus, exponent;
+    // Depending on the first element type, we either have PKCS1 or 2
+    if (topLevelSeq.elements[0].runtimeType == ASN1Integer) {
+      modulus = topLevelSeq.elements[0] as ASN1Integer;
+      exponent = topLevelSeq.elements[1] as ASN1Integer;
+    } else {
+      var publicKeyBitString = topLevelSeq.elements[1];
+
+      var publicKeyAsn = new ASN1Parser(publicKeyBitString.contentBytes()!);
+      ASN1Sequence publicKeySeq = publicKeyAsn.nextObject() as ASN1Sequence;
+      modulus = publicKeySeq.elements[0] as ASN1Integer;
+      exponent = publicKeySeq.elements[1] as ASN1Integer;
+    }
+
+    c1.RSAPublicKey rsaPublicKey =
+        c1.RSAPublicKey(modulus.valueAsBigInteger, exponent.valueAsBigInteger);
+
+    return rsaPublicKey;
+  }
+
+  /// Sign plain text with Private Key
+  ///
+  /// Given a plain text [String] and a [RSAPrivateKey], decrypt the text using
+  /// a [RSAEngine] cipher
+  static String sign(String plainText, c1.RSAPrivateKey privateKey) {
+    var signer = c1.RSASigner(c1.SHA256Digest(), "0609608648016503040201");
+    signer.init(true, c1.PrivateKeyParameter<c1.RSAPrivateKey>(privateKey));
+    return base64Encode(
+        signer.generateSignature(createUint8ListFromString(plainText)).bytes);
+  }
+
+  /// Creates a [Uint8List] from a string to be signed
+  static Uint8List createUint8ListFromString(String s) {
+    var codec = Utf8Codec(allowMalformed: true);
+    return Uint8List.fromList(codec.encode(s));
+  }
+
+  /// Decode Private key from PEM Format
+  ///
+  /// Given a base64 encoded PEM [String] with correct headers and footers, return a
+  /// [RSAPrivateKey]
+  static c1.RSAPrivateKey parsePrivateKeyFromPem(pemString) {
+    List<int> privateKeyDER = decodePEM(pemString);
+    var asn1Parser = new ASN1Parser(Uint8List.fromList(privateKeyDER));
+    var topLevelSeq = asn1Parser.nextObject() as ASN1Sequence;
+
+    var modulus, privateExponent, p, q;
+    // Depending on the number of elements, we will either use PKCS1 or PKCS8
+    if (topLevelSeq.elements.length == 3) {
+      var privateKey = topLevelSeq.elements[2];
+
+      asn1Parser = new ASN1Parser(privateKey.contentBytes()!);
+      var pkSeq = asn1Parser.nextObject() as ASN1Sequence;
+
+      modulus = pkSeq.elements[1] as ASN1Integer;
+      privateExponent = pkSeq.elements[3] as ASN1Integer;
+      p = pkSeq.elements[4] as ASN1Integer;
+      q = pkSeq.elements[5] as ASN1Integer;
+    } else {
+      modulus = topLevelSeq.elements[1] as ASN1Integer;
+      privateExponent = topLevelSeq.elements[3] as ASN1Integer;
+      p = topLevelSeq.elements[4] as ASN1Integer;
+      q = topLevelSeq.elements[5] as ASN1Integer;
+    }
+
+    c1.RSAPrivateKey rsaPrivateKey = c1.RSAPrivateKey(
+        modulus.valueAsBigInteger,
+        privateExponent.valueAsBigInteger,
+        p.valueAsBigInteger,
+        q.valueAsBigInteger);
+
+    return rsaPrivateKey;
+  }
+
+  static List<int> decodePEM(String pem) {
+    return base64.decode(removePemHeaderAndFooter(pem));
+  }
+
+  static String removePemHeaderAndFooter(String pem) {
+    var startsWith = [
+      "-----BEGIN PUBLIC KEY-----",
+      "-----BEGIN RSA PRIVATE KEY-----",
+      "-----BEGIN RSA PUBLIC KEY-----",
+      "-----BEGIN PRIVATE KEY-----",
+      "-----BEGIN PGP PUBLIC KEY BLOCK-----\r\nVersion: React-Native-OpenPGP.js 0.1\r\nComment: http://openpgpjs.org\r\n\r\n",
+      "-----BEGIN PGP PRIVATE KEY BLOCK-----\r\nVersion: React-Native-OpenPGP.js 0.1\r\nComment: http://openpgpjs.org\r\n\r\n",
+    ];
+    var endsWith = [
+      "-----END PUBLIC KEY-----",
+      "-----END PRIVATE KEY-----",
+      "-----END RSA PRIVATE KEY-----",
+      "-----END RSA PUBLIC KEY-----",
+      "-----END PGP PUBLIC KEY BLOCK-----",
+      "-----END PGP PRIVATE KEY BLOCK-----",
+    ];
+    bool isOpenPgp = pem.indexOf('BEGIN PGP') != -1;
+
+    pem = pem.replaceAll(' ', '');
+    pem = pem.replaceAll('\n', '');
+    pem = pem.replaceAll('\r', '');
+
+    for (var s in startsWith) {
+      s = s.replaceAll(' ', '');
+      if (pem.startsWith(s)) {
+        pem = pem.substring(s.length);
+      }
+    }
+
+    for (var s in endsWith) {
+      s = s.replaceAll(' ', '');
+      if (pem.endsWith(s)) {
+        pem = pem.substring(0, pem.length - s.length);
+      }
+    }
+
+    if (isOpenPgp) {
+      var index = pem.indexOf('\r\n');
+      pem = pem.substring(0, index);
+    }
+
+    return pem;
+  }
+
+  /// Encode Private key to PEM Format
+  ///
+  /// Given [RSAPrivateKey] returns a base64 encoded [String] with standard PEM headers and footers
+  String encodePrivateKeyToPemPKCS1(c1.RSAPrivateKey privateKey) {
+    var topLevel = new ASN1Sequence();
+
+    var version = ASN1Integer(BigInt.from(0));
+    var modulus = ASN1Integer(privateKey.n!);
+    var publicExponent = ASN1Integer(privateKey.exponent!);
+    var privateExponent = ASN1Integer(privateKey.d!);
+    var p = ASN1Integer(privateKey.p!);
+    var q = ASN1Integer(privateKey.q!);
+    var dP = privateKey.d! % (privateKey.p! - BigInt.from(1));
+    var exp1 = ASN1Integer(dP);
+    var dQ = privateKey.d! % (privateKey.q! - BigInt.from(1));
+    var exp2 = ASN1Integer(dQ);
+    var iQ = privateKey.q!.modInverse(privateKey.p!);
+    var co = ASN1Integer(iQ);
+
+    topLevel.add(version);
+    topLevel.add(modulus);
+    topLevel.add(publicExponent);
+    topLevel.add(privateExponent);
+    topLevel.add(p);
+    topLevel.add(q);
+    topLevel.add(exp1);
+    topLevel.add(exp2);
+    topLevel.add(co);
+
+    var dataBase64 = base64.encode(topLevel.encodedBytes);
+
+    return """-----BEGIN PRIVATE KEY-----\r\n$dataBase64\r\n-----END PRIVATE KEY-----""";
+  }
+
+  /// Encode Public key to PEM Format
+  ///
+  /// Given [RSAPublicKey] returns a base64 encoded [String] with standard PEM headers and footers
+  String encodePublicKeyToPemPKCS1(c1.RSAPublicKey publicKey) {
+    var topLevel = new ASN1Sequence();
+
+    topLevel.add(ASN1Integer(publicKey.modulus!));
+    topLevel.add(ASN1Integer(publicKey.exponent!));
+
+    var dataBase64 = base64.encode(topLevel.encodedBytes);
+    return """-----BEGIN PUBLIC KEY-----\r\n$dataBase64\r\n-----END PUBLIC KEY-----""";
+  }
+}
+
+Future<void> cryptoTest() async {
+  final algorithm = Cryptography.instance.x25519();
+
+  // Let's generate two keypairs.
+  final keyPair = await algorithm.newKeyPair();
+  final remoteKeyPair = await algorithm.newKeyPair();
+  final remotePublicKey = await remoteKeyPair.extractPublicKey();
+  final remotePrivateKey = await remoteKeyPair.extractPrivateKeyBytes();
+  final myPublicKey = await keyPair.extractPublicKey();
+  final myPrivateKey = await keyPair.extractPrivateKeyBytes();
+
+  // We can now calculate the shared secret key
+  final sharedSecretKey = await algorithm.sharedSecretKey(
+    keyPair: keyPair,
+    remotePublicKey: remotePublicKey,
+  );
+  final skBytes = await sharedSecretKey.extractBytes();
+  print(
+      "public key (A): ${base64.encode(myPublicKey.bytes)} type: ${myPublicKey.type}");
+  print("private key (A): ${base64.encode(myPrivateKey)} ");
+  print(
+      "public key (B): ${base64.encode(remotePublicKey.bytes)} type: ${remotePublicKey.type} ");
+  print("private key (B): ${base64.encode(remotePrivateKey)} ");
+  print("secretkey: ${base64.encode(skBytes)} ");
+
+  final ed = Ed25519();
+  // Sign a message
+  final message = <int>[1, 2, 3];
+  final signature = await ed.sign(
+    message,
+    keyPair: keyPair,
+  );
+  print('Signature bytes: ${signature.bytes}');
+  print('Public key: ${signature.publicKey.toString()}');
+
+  // Anyone can verify the signature
+  final isSignatureCorrect = await ed.verify(
+    message,
+    signature: signature,
+  );
+  print("is correct $isSignatureCorrect");
+}
+
+main() async {
+  var x = AirId(airId: "gfh");
+  getIpAddr().then((ip) {
+    print(ip);
+  });
+  print(getDeviceName());
+  //bonjourTest();
+  await cryptoTest();
 }
